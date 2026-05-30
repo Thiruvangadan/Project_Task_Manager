6467b08565059951d4231139434780a39ef6370b
Ashwanth <tashwanth63@gmail.com>
2026-05-30 11:46:18 +0530

    Ready for deployment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,15 +4,19 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const db = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASS,
+  // port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 try {
-  await db.connect();
+  await db.query("SELECT NOW()");
   console.log("Database connected successfully");
 } catch (error) {
   console.error("Database connection failed", error);
